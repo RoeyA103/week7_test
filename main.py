8ad9f5d4a8e7a27af1644622e5f5ca82cfdfd50d
roey_averbuch <roey6922@gmail.com>
2025-12-04 11:35:17 +0200

    fixing csv name

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ orders = pd.read_json("orders_simple.json")
 
 orders = data_frame_conversion(orders)
 
+
 orders.items_html = cleanhtml_column(orders.items_html)
 
 orders.coupon_used = fill_empty_col(orders.coupon_used , "no coupon")
 
 orders = create_ordermonth_col(orders)
-
 
 orders = create_high_value_order(orders)
 
@@ -20,8 +20,9 @@ orders = sort_by_total_amount(orders)
 
 orders = create_mean_rating_col(orders)
 
+
 orders = filter_by_amount_rating(orders)
 
 orders = create_delivery_status_col(orders)
 
-orders.to_csv("clean_orders_[ID_NUMBER].csv")
+orders.to_csv("clean_orders_[315329995].csv")
